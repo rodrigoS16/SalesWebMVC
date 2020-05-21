499c262821084770a88124ba4b42598f6972b7b6
Rodrigo Castilho <rodrigoS16Unicesumar@gmail.com>
2020-05-21 16:46:29 -0300

    Add sellerService and basic findAll

---
+++ SalesWebMvc/Models/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using SalesWebMvc.Data;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _Context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _Context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _Context.Seller.ToList();
+        }
+    }
+}
